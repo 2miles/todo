0c5bcf07e725125f2e1db0b01beea5bd6ba027b5
miles <miles4599@gmail.com>
2022-04-20 13:37:58 -0700

    added todo model

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    start_date = models.DateTimeField(auto_now_add=True)
+    finish_date = models.DateField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    # one user can have many todos
+    # if a user gets deleted their todos get deleted too
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self): 
+        return self.title
+
+    def get_absolute_url(self):
+        pass
+        # return reverse("todo_detail", kwargs={"pk": self.pk})

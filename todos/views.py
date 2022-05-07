1c0ff02002607f8c3bd4032685edbd44f2f63229
miles <miles4599@gmail.com>
2022-05-06 20:45:49 -0700

    added ability for user to add a new todo item

--- todos/views.py
+++ todos/views.py
@@ -1,23 +1,24 @@
-
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 
 from .models import Todo
+
 # Create your views here.
 
-class TodoListView(ListView): 
+
+class TodoListView(ListView):
     model = Todo
-    template_name = 'todo_list.html'
+    template_name = "todo_list.html"
 
     def get_queryset(self):
-      """Returns Todos that belong to the current user, oldest first"""
-      return Todo.objects.filter(user=self.request.user).order_by('start_date')
+        """Returns Todos that belong to the current user, oldest first"""
+        return Todo.objects.filter(user=self.request.user).order_by("start_date")
 
 
 class TodoDetailView(DetailView):
     model = Todo
-    template_name = 'todo_detail.html'
+    template_name = "todo_detail.html"
 
     def get_queryset(self):
         return Todo.objects.filter(user=self.request.user)
@@ -25,16 +26,26 @@ class TodoDetailView(DetailView):
 
 class TodoUpdateView(UpdateView):
     model = Todo
-    fields = (
-        "title",
-        "description", 
-        "completed"
-    )
-    template_name = 'todo_edit.html'
+    fields = ("title", "description", "completed")
+    template_name = "todo_edit.html"
     success_url = reverse_lazy("todo_list")
-  
+
 
 class TodoDeleteView(DeleteView):
     model = Todo
-    template_name = 'todo_delete.html'
+    template_name = "todo_delete.html"
     success_url = reverse_lazy("todo_list")
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    template_name = "todo_new.html"
+    fields = (
+        "title",
+        "description",
+    )
+    success_url = reverse_lazy("todo_list")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TodoCreateView, self).form_valid(form)
